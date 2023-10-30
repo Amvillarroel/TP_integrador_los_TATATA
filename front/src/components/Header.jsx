350ef988627ed74ad77917f38d066299b387b286
Carlos Soto <carloss.empire@gmail.com>
2023-10-30 20:47:25 -0300

    Consultando a la API para traer una lista de las 20 peliculas mas populares

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <section style={{height:'68px', display:'flex', gap:'8px', justifyContent:'space-between', alignItems:'center'}}>
                     <picture>
                         <a style={{display:'inline-block'}} href="#">
-                            <img style={{maxWidth:'150px'}} src="./public/Netflix_2015_logo.svg.png" alt="logo" />
+                            <img style={{maxWidth:'150px'}} src="/Netflix_2015_logo.svg.png" alt="logo" />
                         </a>
                     </picture>
                     <ul style={{ margin:'0', listStyle:'none', display:'flex', gap:'8px', justifyContent:'space-between'}}>
